f8821959f132a7161be0058ee100a84c11c662a0
sekhubede <43635891+sekhubede@users.noreply.github.com>
2023-06-07 16:36:45 +0200

    Running some test logic in the Game.cs class
    Just to see how to read the key pressed by the user. We will remove this code, it is just for testing how we would read the key pressed by the user using the ConsoleKeyInfo method.

--- ConsoleUI/Game.cs
+++ ConsoleUI/Game.cs
@@ -8,5 +8,31 @@ namespace ConsoleUI
 {
     internal class Game
     {
+        public void Start()
+        {
+            Console.WriteLine("The game is starting!");
+
+            ConsoleKeyInfo keyPressed = Console.ReadKey();
+            if (ConsoleKey.Enter == keyPressed.Key)
+            {
+                Console.WriteLine("You pressed ENTER");
+            }
+
+            if (ConsoleKey.UpArrow == keyPressed.Key)
+            {
+                Console.WriteLine("You pressed the UP ARROW");
+            }
+
+            WaitForKey();
+        }
+
+
+
+        public void WaitForKey()
+        {
+            Console.WriteLine("\npress any key...");
+            Console.ReadKey(true);
+        }
+
     }
 }
