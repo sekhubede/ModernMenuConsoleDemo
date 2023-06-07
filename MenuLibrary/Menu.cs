4a2af2f297b3b323533f5a01000cd2aed3bb8909
sekhubede <43635891+sekhubede@users.noreply.github.com>
2023-06-07 16:41:22 +0200

    Added some logic to the Menu.cs class in the MenuLibrary
    Added fields and a constructor that takes in two parameters to define the menu.

--- MenuLibrary/Menu.cs
+++ MenuLibrary/Menu.cs
@@ -8,6 +8,19 @@ namespace MenuLibrary
 {
     public class Menu
     {
+        // Adding fields for the menu
+        private int SelectetIndex;
+        private string[] Options;
+        private string Prompt;
 
+        // Adding a constructor with two parameters
+        public Menu(string prompt, string[] options)
+        {
+            this.Prompt = prompt;
+            this.Options = options;
+            this.SelectetIndex = 0;
+        }
+
+       
     }
 }
