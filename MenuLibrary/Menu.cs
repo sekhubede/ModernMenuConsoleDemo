a7e22ed169d3f332257a198675046a005c0debd8
sekhubede <43635891+sekhubede@users.noreply.github.com>
2023-06-08 09:01:44 +0200

    Cleaned up the logic a little
    - Changed the access modifier of the DisplayOptions method to private.
    - Added a Run method that will be responsible for iterating through the options and returning the selected index when the user presses enter depending on the option they have selected.

--- MenuLibrary/Menu.cs
+++ MenuLibrary/Menu.cs
@@ -8,12 +8,12 @@ namespace MenuLibrary
 {
     public class Menu
     {
-        // Adding fields for the menu
+        // Fields for the menu
         private int SelectetIndex;
         private string[] Options;
         private string Prompt;
 
-        // Adding a constructor with two parameters
+        // A constructor with two parameters to define our menu
         public Menu(string prompt, string[] options)
         {
             this.Prompt = prompt;
@@ -21,6 +21,37 @@ namespace MenuLibrary
             this.SelectetIndex = 0;
         }
 
-       
+        // This method that will be resposible for rendering the menu
+       private void DisplayOptions()
+        {
+            Console.WriteLine(Prompt);
+
+            for (int i = 0; i < Options.Length; i++)
+            {
+                string currentOption = Options[i];
+                string prefix;
+
+                if (i == SelectetIndex)
+                {
+                    prefix = "*";
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.BackgroundColor = ConsoleColor.White;
+                }
+                else
+                {
+                    prefix = " ";
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.BackgroundColor = ConsoleColor.Black;
+                }
+
+
+                Console.WriteLine($"{prefix} << {currentOption} >>");
+            }
+        }
+
+        public int Run()
+        {
+
+        }
     }
 }
